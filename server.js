c28579bf5619eb361a30e171c5ef9b8dc7657ef8
Richard Oh <rso136@gmail.com>
2016-09-11 19:27:01 -0400

    Corrected port to process.env.PORT

--- server.js
+++ server.js
@@ -166,9 +166,5 @@ app.post('/update/:id/:rating', function(req, res) {
     })
 });
 
-
-
-// listen on port 3000
-app.listen(3000, function() {
-    console.log('App running on port 3000!');
-});
+var PORT = process.env.PORT || 3000;
+app.listen(PORT);
