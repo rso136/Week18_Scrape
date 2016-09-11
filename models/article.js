ed8632a42857dfce1731d925cbbec0094eeb89ac
Richard Oh <rso136@gmail.com>
2016-09-11 19:07:50 -0400

    Erased extraneous comments

--- models/article.js
+++ models/article.js
@@ -1,16 +1,13 @@
-// require mongoose
 var mongoose = require('mongoose');
-// create Schema class
 var Schema = mongoose.Schema;
 
-// Create article schema
 var ArticleSchema = new Schema({
-  // title is required
+
   title: {
     type:String,
     required:true
   },
-  // link is required
+
   link: {
     type:String,
     required:true
@@ -18,15 +15,13 @@ var ArticleSchema = new Schema({
   rating: {
     type:Number, min:0, max:5
   },
- // this only saves one note's ObjectId. ref refers to the Note model.
+
   note: {
       type: Schema.Types.ObjectId,
       ref: 'Note'
   }
 });
 
-// Create the Article model with the ArticleSchema
 var Article = mongoose.model('Article', ArticleSchema);
 
-// export the model
 module.exports = Article;
